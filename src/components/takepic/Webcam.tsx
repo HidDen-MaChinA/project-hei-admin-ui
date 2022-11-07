95dcc6a7687fffd6f0e5ab60904ad3b1e19843e4
hidden-machina <hei.franco.3@gmail.com>
2022-11-07 20:26:14 +0300

    feat:new feature

--- src/components/takepic/Webcam.tsx
+++ src/components/takepic/Webcam.tsx
@@ -1,6 +1,9 @@
+import { ClassNames } from '@emotion/react';
+import { Button } from '@mui/material';
 import React, { Component, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Webcam from "react-webcam";
+import "./Webcam.css"
 const WebcamComponent = () => <Webcam />;
 const videoConstraints = {
     width: 220,
@@ -12,28 +15,32 @@ const WebcamCapture = () => {
 
     const webcamRef = React.useRef<any>(null);
     const [imgSrc, setImgSrc] = React.useState(null);
-    const navigate=useNavigate();
+    const navigate = useNavigate();
 
     const capture = React.useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    setImgSrc(imageSrc);
-    console.log(imageSrc)
-  }, [webcamRef, setImgSrc]);
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImgSrc(imageSrc);
+        console.log(imageSrc)
+    }, [webcamRef, setImgSrc]);
 
 
     return (
         <div className="webcam-container">
-            <Webcam
-                audio={false}
-                height={"50%"}
-                ref={webcamRef}
-                screenshotFormat="image/jpeg"
-                width={"70%"}
-                videoConstraints={videoConstraints}
-            />
-            <button onClick={(e) => { capture() }}>
-                Capture</button>
-                {imgSrc!==null&& <img src={imgSrc}/>}
+            <div className='head'></div>
+            <div className='webcam'>
+                <Webcam
+                    audio={false}
+                    height={"600"}
+                    ref={webcamRef}
+                    screenshotFormat="image/jpeg"
+                    width={"1200"}
+                    videoConstraints={videoConstraints}
+                />
+            </div>
+            <div className='navigation'>
+            <button onClick={(e)=>{navigate("/home")}} className="capture" >Home</button>
+                <button onClick={(e) => { capture() }} className="capture">Capture</button>
+            </div>
         </div>
     );
 };
